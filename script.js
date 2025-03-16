7d80d4b531bc3826cedc311bda91378517dbcb08
Shaniza <shaniza.rr17@gmail.com>
2025-03-16 22:13:22 +0600

    second commit

--- script.js
+++ script.js
@@ -13,3 +13,15 @@ console.log(c === d)//false, string and integer are not equal
 const e = 5;
 const f = 5;
 console.log(e === f)//true, as both value and type is same
+
+const g = 5;
+const h = 10;
+console.log(g != h)//!=	not equal, ans - true
+
+const i = 5;
+const j = "3";
+console.log(i !== j)//!==,  ans - true if not equal value or not equal type
+//>	greater than
+const m = 10; 
+const n = 2;
+console.log(m > n)// true if is greater than n
